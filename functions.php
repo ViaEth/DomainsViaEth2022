ead42f130a70fb1da87f5d2d6fcf5253b93273ec
Via.Eth <contact+dev@viaeth.io>
2022-09-10 22:56:25 -0700

    1. Added a function.php

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+
+//Start of Matomo Tracker Code
+function Matomo_Tags(){
+?>
+	<!-- Matomo Tag Manager -->
+	<!-- End Matomo Tag Manager -->
+<?php
+}
+add_action('wp_head', 'Matomo_Tags');
+//End of Matomo Tracker Code
+
+?>
