861b74886eb69d85bd2e373c0c317e084e603339
Via.Eth <contact+dev@viaeth.io>
2022-12-15 21:55:13 -0800

    1. Added code to enqueue the style.css theme.

--- functions.php
+++ functions.php
@@ -1,5 +1,11 @@
 <?php
 
+function add_style() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'add_style' );
+
+
 //Start of Matomo Tracker Code
 function Matomo_Tags(){
 ?>
